6cdca1975fafeba407a08e4b19155055908003a0
kaarelel <70900290+kaarelel@users.noreply.github.com>
2021-02-19 15:39:29 +0200

    Kodutöö2

---
+++ w02_klass/AnimalMain.java
@@ -0,0 +1,16 @@
+public class AnimalMain {
+    public static void main(String args[]){
+        Animal animal1 = new Animal("RR");
+
+        animal1.AnimalType("Hobune");
+
+        animal1.AnimalAge(9);
+
+        animal1.AnimalBreed("Mustang");
+
+        animal1.AnimalWeight(50.000);
+
+        animal1.printAnimal();
+    }
+}
+    
