50dc39d6c1eb8c1793b2d220c83aaef7d65d8f98
kaarelel <70900290+kaarelel@users.noreply.github.com>
2021-04-15 17:38:37 +0300

    TicTac

---
+++ w07_junit/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        TTT game = new TTT();
+        game.initializeBoard();
+        scanner.close();
+        System.out.println("Trips-Traps-Trull!");
+        do
+        {
+            System.out.println("Mängulaud:");
+            game.printBoard();
+            int row;
+            int column;
+            do
+            {
+                System.out.println("Mängija " + game.getCurrentPlayerMark() + ", sisesta tühi rida ja veerg, kuhu soovid oma nupu asetada!");
+                row = scanner.nextInt()-1;
+                column = scanner.nextInt()-1;
+            }
+            while (!game.placeMark(row, column));
+            game.changePlayer();
+        }
+        while(!game.checkForWin() && !game.isBoardFull());
+        if (game.isBoardFull() && !game.checkForWin())
+        {
+            System.out.println("Jäite viiki!");
+        }
+        else
+        {
+            System.out.println("Mängulaud:");
+            game.printBoard();
+            game.changePlayer();
+            System.out.println(Character.toUpperCase(game.getCurrentPlayerMark()) + " Võidab!");
+        }
+    }
+}
